69b235cc97af3ca909172313b355e7f88e0ac2e7
karstenmonteiro <karsten.monteiro@edu.sait.ca>
2025-03-06 04:18:16 -0700

    updates

--- app/components/About.tsx
+++
@@ -1,7 +0,0 @@
-import GradientText from "./ui/GradientText";
-
-export default function About() {
-  return (
-        <GradientText size="4xl" weight="black" text="ABOUT" />
-    );
-}
--- app/components/Menu.tsx
+++
@@ -1,7 +0,0 @@
-import GradientText from "./ui/GradientText";
-
-export default function Menu() {
-  return (
-        <GradientText size="4xl" weight="black" text="MENU" />
-    );
-}

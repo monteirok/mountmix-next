f4e066063b90da0f75b60dd2e119367bc5b7f2b1
karstenmonteiro <karsten.monteiro@edu.sait.ca>
2025-03-10 02:33:38 -0600

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import "./globals.css";
@@ -15,6 +17,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Navbar />
         {children}
         <Footer />
+
+        {/* Vercel */}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
